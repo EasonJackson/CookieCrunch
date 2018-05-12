a79c644a0fba5e369e8dc593fd0e6d2f8defc774
EasonJackson <pyhanson@gmail.com>
2018-05-12 16:25:12 -0400

    animate score func in game scene

--- CookieCrunch/View/GameScene.swift
+++ CookieCrunch/View/GameScene.swift
@@ -385,6 +385,28 @@ class GameScene: SKScene {
     // 7
     run(SKAction.wait(forDuration: longestDuration), completion: completion)
   }
+  
+  func animateScore(for chain: Chain) {
+    // Figure out what the midpoint of the chain is.
+    let firstSprite = chain.firstCookie().sprite!
+    let lastSprite = chain.lastCookie().sprite!
+    let centerPosition = CGPoint(
+      x: (firstSprite.position.x + lastSprite.position.x)/2,
+      y: (firstSprite.position.y + lastSprite.position.y)/2 - 8)
+    
+    // Add a label for the score that slowly floats up.
+    let scoreLabel = SKLabelNode(fontNamed: "GillSans-BoldItalic")
+    scoreLabel.fontSize = 16
+    scoreLabel.text = String(format: "%ld", chain.score)
+    scoreLabel.position = centerPosition
+    scoreLabel.zPosition = 300
+    cookiesLayer.addChild(scoreLabel)
+    
+    let moveAction = SKAction.move(by: CGVector(dx: 0, dy: 3), duration: 0.7)
+    moveAction.timingMode = .easeOut
+    scoreLabel.run(SKAction.sequence([moveAction, SKAction.removeFromParent()]))
+  }
+
 }
 
 
